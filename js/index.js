70f32a334ba3d1597a836bec1f8a292d970ab3ec
Jacilyn Hayden <jacihayden@gmail.com>
2019-02-03 01:01:24 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+const main = document.getElementById('main');
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
